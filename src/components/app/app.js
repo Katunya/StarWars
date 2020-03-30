7ab8768284d8f153c50e6d5264bd0865d23bbb1b
Ekaterina Dubtsova <e.dubcova@profsoft.pro>
2020-03-30 14:22:21 +0400

    feat: refactor page and item details

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,44 +7,38 @@ import PeoplePage from '../people-page';
 import ErrorBoundry from "../error-boundry";
 
 import './app.css';
+import ItemDetails from "../item-details";
+import Row from "../row";
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true
   };
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
-
   render() {
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+    const {
+      getPerson,
+      getStarship,
+      getPersonImage,
+      getStarshipImage,
+    } = this.swapiService;
+    const personDetails = (
+      <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}/>
+    );
+
+    const starshipDetails = (
+      <ItemDetails itemId={5} getData={getStarship} getImageUrl={getStarshipImage}/>
+    );
 
     return (
       <ErrorBoundry>
         <div className="stardb-app">
-          <Header />
-          { planet }
-
-          <div className="row mb2 button-row">
-            <button
-              className="toggle-planet btn btn-warning btn-lg"
-              onClick={this.toggleRandomPlanet}>
-              Toggle Random Planet
-            </button>
-            <ErrorButton />
-          </div>
-
-          <PeoplePage />
-
+          <Header/> <Row left={personDetails} right={starshipDetails}/>
         </div>
       </ErrorBoundry>
     );
